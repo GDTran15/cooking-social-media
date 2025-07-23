60e8f66fc26e3360d5bd239338ed69a3a08af2dc
GDTran15 <trangiaiduong1512@gmail.com>
2025-07-23 13:37:32 +1000

    defaul

---
+++ src/main/java/com/cooking/cooking_social_media/CookingSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.cooking.cooking_social_media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingSocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingSocialMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cooking/cooking_social_media/CookingSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooking.cooking_social_media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingSocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
