3dba2dac484ceb08f7443af38cd02dc955f2550f
GDTran15 <trangiaiduong1512@gmail.com>
2025-07-23 15:20:23 +1000

    add User

--- src/main/java/com/cooking/cooking_social_media/model/User.java
+++ src/main/java/com/cooking/cooking_social_media/model/User.java
@@ -1,4 +1,22 @@
 package com.cooking.cooking_social_media.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+    private String userName;
+
+    @OneToMany(mappedBy = "user")
+    private List<Recipe> recipes;
 }
