0582e940bcdebfa8263898af889d8500906c34c8
GDTran15 <trangiaiduong1512@gmail.com>
2025-07-24 16:14:09 +1000

    add dto for loggin request and response

---
+++ src/main/java/com/cooking/cooking_social_media/DTO/LoginRequestDTO.java
@@ -0,0 +1,6 @@
+package com.cooking.cooking_social_media.DTO;
+
+public record LoginRequestDTO(
+        String userName,
+        String password) {
+}
---
+++ src/main/java/com/cooking/cooking_social_media/DTO/LoginResponseDTO.java
@@ -0,0 +1,9 @@
+package com.cooking.cooking_social_media.DTO;
+
+
+public record LoginResponseDTO(
+        String token,
+        Integer customerID) {
+
+
+}
